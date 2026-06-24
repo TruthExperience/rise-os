20e9c37979e2b7d4575d99174fa5fe449ac2b631
TruthExperience <amwade537@gmail.com>
2026-06-24 13:02:09 -0400

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { AuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+  // paste whatever else is in your current [...nextauth]/route.ts here
+};
