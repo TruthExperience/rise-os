c3af7913dff4c5d64946b8d27e51e3db1e7a6ec7
TruthExperience <amwade537@gmail.com>
2026-09-16 05:43:22 -0400

    Add additional path checks for user authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -61,7 +61,9 @@ export async function middleware(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/api')
+    !request.nextUrl.pathname.startsWith('/api') &&
+    !request.nextUrl.pathname.startsWith('/directory') &&
+    !request.nextUrl.pathname.startsWith('/l/')
   ) {
     const url = request.nextUrl.clone()
     url.pathname = '/login'
